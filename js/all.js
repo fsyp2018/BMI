dce34c1d5e757c10689c6cb504e61687e9c6cf5b
fsyp2018 <45746305+fsyp2018@users.noreply.github.com>
2018-12-20 22:59:22 +0800

    Add files via upload

--- js/all.js
+++ js/all.js
@@ -1,31 +1,63 @@
 var btn = document.querySelector('.rbut');
+var now = new Date();
+var totalresult = JSON.parse(localStorage.getItem('BMIlist')) || [];
 
-function saveBMI(e) {
+function saveBMI() {
+	//取得身高體重數值
 	var heightrs = document.querySelector('.height').value;
-    var weightrs = document.querySelector('.weight').value;
-	if (heightrs !=="" && weightrs !==""){
-		    var hwresult =(parseFloat(weightrs) / parseFloat(Math.pow((heightrs/100),2))).toFixed(1);
+	var weightrs = document.querySelector('.weight').value;
+	//判斷是否有輸入身高體重數值，並算出BMI
+	if (heightrs !== "" && weightrs !== "") {
+		var hwresult = (parseFloat(weightrs) / parseFloat(Math.pow((heightrs / 100), 2))).toFixed(1);
 	} else {
 		alert("請輸入身高或體重");
+		return
 	}
+	//判斷BMI肥胖度
 	if (hwresult >= 40) {
-		alert("重度肥胖");
+		var obesity = '重度肥胖';
+		var grade = 'lev6';
 	} else if (hwresult >= 35) {
-		alert("中度肥胖");		
+		var obesity = '中度肥胖';
+		var grade = 'lev5';
 	} else if (hwresult >= 30) {
-		alert("輕度肥胖");		
+		var obesity = '輕度肥胖';
+		var grade = 'lev4';
 	} else if (hwresult >= 25) {
-		alert("過重");		
+		var obesity = '過重';
+		var grade = 'lev3';
 	} else if (hwresult >= 18.5) {
-		alert("理想");		
+		var obesity = '理想';
+		var grade = 'lev2';
 	} else if (hwresult <= 18.4) {
-		alert("過輕");		
+		var obesity = '過輕';
+		var grade = 'lev1';
 	}
-
-    localStorage.setItem('height',heightrs);
-    localStorage.setItem('weight',weightrs);
-    localStorage.setItem('hwresult',hwresult);
+	//取得現在時間
+	var nowtime = now.getFullYear() + '年' + (now.getMonth() + 1) + '月' + now.getDate() + '日' + now.getHours() + '時' + now.getMinutes() + '分' + now.getSeconds() + '秒';
+	console.log(obesity);
+	console.log(grade);
+	console.log(nowtime);
+	localStorage.setItem('height', heightrs);
+	localStorage.setItem('weight', weightrs);
+	localStorage.setItem('hwresult', hwresult);
+	//將取得的數值存進陣列
+	var todo = {
+		height: heightrs,
+		weight: weightrs,
+		BMI: hwresult,
+		nowtime: nowtime,
+		obesity: obesity,
+		grade: grade
+	}
+	console.log(todo);
+	//將陣列以JSON方式輸入至localstorage
+	totalresult.push(todo);
+	localStorage.setItem('BMIlist', JSON.stringify(totalresult));
 }
 
+function updatelist(){
 
+}
+//監聽看結果按鈕
 btn.addEventListener('click', saveBMI);
